f561a671046104501b42d2a9f890b7b5046edea8
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-25 09:50:48 -0600

    updates color

--- Week 1/ponder/dom.js
+++ Week 1/ponder/dom.js
@@ -27,5 +27,6 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
+    document.getElementById('code').style.color = 'red';
 })
                 
