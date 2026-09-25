d317928e23101161ab86a54acd97c5649831534d
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-25 09:38:53 -0600

    background color

--- Week 1/ponder/dom.js
+++ Week 1/ponder/dom.js
@@ -11,3 +11,9 @@ document.getElementById('topics').style.color = 'purple';
 let list=document.querySelector('list');
 
 list.style.border = '3px solid black';
+
+let paragraphs = document.querySelectorAll('p');
+
+//para.style.backgroundColor = 'lightblue';
+
+para.classList.add('background');
